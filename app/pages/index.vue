2d38697a692ef683b93e8a9fdc0a267d3040aab7
Patrick Luz <eu@patrickluz.dev>
2025-11-16 18:00:54 -0300

    chore: add shadcnui

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Index page</h1>
+    <div>
+        <Button>Click me</Button>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/hints',
     '@nuxt/icon',
-    '@nuxt/image'
-  ]
+    '@nuxt/image',
+    'shadcn-nuxt',
+    '@vueuse/nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  }
 })
